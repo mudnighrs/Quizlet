4529f09f6c9a8149725f960c46a866468eaf219d
Lakshaya Singh Tanwar <tanwarlakshay21@gmail.com>
2025-07-08 20:20:17 +0530

    Initial commit

---
+++ Quizlet/Model/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Quizlet
+//
+//  Created by Lakshaya Singh Tanwar on 01/07/25.
+//
+struct Question{
+    let text : String
+    let answer : String
+    init(q: String, a: String) {
+        text = q
+        answer = a
+    }
+}
---
+++ QuizletTests/QuizletTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuizletTests.swift
+//  QuizletTests
+//
+//  Created by Lakshaya Singh Tanwar on 01/07/25.
+//
+
+import Testing
+@testable import Quizlet
+
+struct QuizletTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
